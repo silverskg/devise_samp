2211828a56a8837d2bc134fb10c3b088b62056bc
silverskg <chokichoki_gm@i.softbank.jp>
2020-07-17 22:47:41 +0900

    deviseログイン機能実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+  before_action :controller_permitted_parameters, if: :devise_controller?
+
+  def controller_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'items#index'
-  resources :item
+  resources :items
+
+  resources :user, only: :show
 end
